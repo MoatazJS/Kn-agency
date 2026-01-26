3d06e0c6a28c4906b2247aab299c22e4cce8c730
unknown <moatazmamdouh.js@gmail.com>
2026-01-26 13:27:01 +0200

    creating layout of hero section

--- myComponents/PagesComponents/HomeComponents/HeroSection.tsx
+++ myComponents/PagesComponents/HomeComponents/HeroSection.tsx
@@ -3,11 +3,19 @@ import React from "react";
 export default function HeroSection() {
   return (
     <>
-      <section className="min-h-screen flex bg-kn-white items-center justify-center">
-        <div className="bg-red-500 w-20 h-20 ">Red</div>
-        <div className="bg-blue-500 w-20 h-20">Blue</div>
+      <section className="min-h-screen bg-kn-white flex justify-center flex-col text-center">
+        <div className="text-black flex justify-between flex-col">
+          <h1 className="text-7xl font-bold">Lorem, ipsum dolor.</h1>
+          <h1 className="text-7xl font-bold">Lorem, ipsum dolor.</h1>
+          <p className="text-2xl font-light">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
+            nemo aperiam assumenda odit amet!
+          </p>
+        </div>
+        <div className="bg-black">
+          <button>See our work</button>
+        </div>
       </section>
-      <section></section>
     </>
   );
 }
