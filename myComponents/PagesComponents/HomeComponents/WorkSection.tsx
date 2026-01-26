b5b1460d610747124c6e7e31baeadcbb93589e3c
unknown <moatazmamdouh.js@gmail.com>
2026-01-26 15:44:33 +0200

    created work section layout

--- myComponents/PagesComponents/HomeComponents/WorkSection.tsx
+++ myComponents/PagesComponents/HomeComponents/WorkSection.tsx
@@ -1,5 +1,28 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 import React from "react";
 
 export default function WorkSection() {
-  return <></>;
+  return (
+    <>
+      <section className="py-32 bg-kn-black text-kn-white px-6 md:px-12">
+        <div className="container mx-auto">
+          <div className="flex flex-col md:flex-row justify-between items-end mb-24">
+            <h2 className="text-4xl md:text-6xl font-bold font-heading uppercase tracking-tight mb-8 md:mb-0">
+              Selected <br /> Work
+            </h2>
+            <Link href="/work">
+              <Button
+                variant="secondary"
+                className="border-kn-white text-kn-white hover:bg-kn-white hover:text-kn-black"
+              >
+                View All Projects
+              </Button>
+            </Link>
+          </div>
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-8">cards</div>
+        </div>
+      </section>
+    </>
+  );
 }
